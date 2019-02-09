b7ac6d592001c1faaf14608a43da4d41e49299b5
Snarkorel <snark-57@mail.ru>
2019-02-09 15:59:08 +0300

    * Response: class is public abstract

--- TransnaviClient/response/Response.cs
+++ TransnaviClient/response/Response.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TransnaviClient.response
+﻿namespace TransnaviClient.response
 {
-    class Response
+    public abstract class Response
     {
+        //TODO
     }
 }
