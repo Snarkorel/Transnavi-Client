49bc4c0a86b0e270a0f571b52de8a57d077792ca
Snarkorel <snark-57@mail.ru>
2019-02-09 15:57:08 +0300

    * Added generic Request and Response (not implemented yet)

---
+++ TransnaviClient/request/Request.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace TransnaviClient.request
+{
+    public abstract class Request
+    {
+        [JsonProperty("id")]
+        public int RequestId { get; set; }
+        [JsonProperty("jsonrpc")]
+        public string JsonRpcVersion { get; set; }
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        public Request(int requestId, string method)
+        {
+            JsonRpcVersion = "2.0";
+            RequestId = requestId;
+            Method = method;
+        }
+    }
+}
